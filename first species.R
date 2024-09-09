0a3ae7097bb1f1e178e932b99eb8e2e5ea6efa87
louiseebetse <lestebe@bluewin.ch>
2024-09-09 16:58:51 +0200

    try to clean the plant data

---
+++ first species.R
@@ -0,0 +1,39 @@
+library(virtualspecies)
+library(terra)
+library(geodata)
+library(git2r)
+library(sf)
+library(gtools) # V.3.9.4
+library(data.table)
+library(sp)
+
+
+#set the session to the right place
+setwd("/Users/louise/Desktop/UNIL/Master/Rstudio/Virtual species")
+
+#export my data from my file
+load("SPImaster/sp/1008910")
+swiss_shape<-st_read("swiss_shapefile.gpkg")
+
+class(my.sp)
+SpatialPoints(my.sp[my.sp$x])
+coordinates(C.kitaibelii) <- c("x", "y")
+coordinates(my.sp) <- c("x", "y")
+C.kitaibelii<- remove.duplicates(C.kitaibelii, zero = 300)
+plot(C.kitaibelii)
+mask(swissclim,swiss_shape, inverse= FALSE)
+
+
+clear<- function(species){
+ species$date <- as.IDate(species$date)
+ species<- species[species$v_presence_status==2,]
+ species<- species[species$v_introduction_status!=4,]
+ species<- species[species$v_doubt_status<2,]
+ species<- species[species$v_xy_radius<= 100,]
+ species <- species[!is.na(species$x), ]
+ species<- species[species$date>=2004,]
+ return(species)
+}
+
+C.kitaibelii<-clear(my.sp)
+ 
