2a0467b5f65127ef214426a251d7c4844b9d4ba8
louiseebetse <lestebe@bluewin.ch>
2024-09-09 18:55:48 +0200

    clear the plant data (1sp), extract the presence coordinates in my variables -> do glm find the formula -> read the notes

--- first species.R
+++ first species.R
@@ -11,18 +11,39 @@ library(sp)
 #set the session to the right place
 setwd("/Users/louise/Desktop/UNIL/Master/Rstudio/Virtual species")
 
-#export my data from my file
-load("SPImaster/sp/1008910")
+
+#export my variables from my file
+my_variables <- mixedsort(list.files(path = "variables",
+                                     pattern = NULL,
+                                     full.names = TRUE))
+#rasterise my documents
+my_variables<- rast(my_variables)
+cat(crs(my_variables))
+#change the names of my variables
+names(my_variables) = c("ai","bio4", "bio6", "bio15","gdd3","pop2000", "pop5000", "rad", "dem", "slope", "canopy")
+#compute the curvature
+my_variables$dem<-terrain(my_variables$dem, v= "TPI")
+#plot a raster
+plot(my_variables)
+#export the swiss shapefile
 swiss_shape<-st_read("swiss_shapefile.gpkg")
 
+#export my data from my file
+load("SPImaster/sp/1008910")
 C.kitaibelii<-clear(my.sp)
-coordinates(C.kitaibelii) <- c("x", "y")
+C.kitaibelii <- SpatialPointsDataFrame(coords=C.kitaibelii[,6:7], data=C.kitaibelii[ , -c(6, 7)])
 C.kitaibelii<- remove.duplicates(C.kitaibelii, zero = 300)
+C.kitaibelii<- as.data.frame(C.kitaibelii)
+coord<- C.kitaibelii[,25:26]
 #plot(C.kitaibelii)
 
-mask(swissclim,swiss_shape, inverse= FALSE)
+#project the species coordinates
+plot(my_variables$bio15)
+points(C.kitaibelii)
+variablesValues<-data.frame(extract(my_variables,coord))
+head(variablesValues)
 
-
+#function to clear the data
 clear<- function(species){
  species$date <- as.IDate(species$date)
  species<- species[species$v_presence_status==2,]
@@ -31,6 +52,8 @@ clear<- function(species){
  species<- species[species$v_xy_radius<= 100,]
  species <- species[!is.na(species$x), ]
  species<- species[species$date>=2004,]
+ species<- species[species$v_co_canton!="IT-21",]
+ species<- species[species$v_co_canton!="IT-25",]
  return(species)
 }
 
