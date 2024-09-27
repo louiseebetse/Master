0d97cc775bb662521fc34f50c4fce8043ff00cee
louiseebetse <lestebe@bluewin.ch>
2024-09-27 11:29:30 +0200

    1st formula for virtual species

--- first species.R
+++ first species.R
@@ -8,7 +8,8 @@ library(data.table)
 library(sp)
 library(CovSel)
 library(np)
-
+library(biomod2)
+library(MASS)
 
 #set the session to the right place
 setwd("/Users/louise/Desktop/UNIL/Master/Rstudio/Virtual species")
@@ -55,10 +56,114 @@ head(spData)
 spData<-spData[,-3]#remove the id line
 spData$presence <- rep(1, times = length(spData$x))#put a presence line
 
-#can't use it
-args<- cov.sel(T=spData$presence, Y= spData$x, X= spData[,3:13], type = "np")
+#generate background points 
+bg_rand <- terra::spatSample(my_variables$ai, 10000, "random", na.rm=T, as.points=TRUE)
+plot(swiss_shape$geom,col='lightgrey')
+points(coord,pch='+',col='red')# one way to plot presences
+points(C.kitaibelii$x, C.kitaibelii$y, col='red',  pch=19) #two ways to plot presences
+points(bg_rand,pch=13,cex=0.3)
 
-glmStart<- glm(spData$presence~1, data=spData, family= "binomial")
+#create a dataframe with presences and pseudo absences
+sp_env <- data.frame(coord, occ=1)# create dataframe with presence
+bg_rand_buf_df <- data.frame(terra::geom(bg_rand)[,c('x','y')])# create dataframe with absence
+bg_rand_buf_df$occ <- 0 #add the 0 to the dataframe with absences
+sp_env <- rbind(sp_env, bg_rand_buf_df) #bind the two dataframe in one big 
+sp_env <- cbind(sp_env, terra::extract(x = my_variables , y = sp_env[,c('x','y')], cells=T) )
+
+#sp_env<-sp_env[,-9]
+sp_env <- na.omit(sp_env)# remove the NA of the data
+#test<- sp_env[c(1:200,900:1100),]
+#args<- cov.sel(T=sp_env$occ, Y= sp_env$x , X= sp_env[,4:14], type = "np", alpha = 0.3)
+#glmModAIC<-stepAIC(glmStart, glm.formula, direction= "both", trace= FALSE, k=2)
+#summary(args)
+
+#create the glm
+glm.formula<- bm_MakeFormula("sp_env$occ",sp_env[, c("bio6", "canopy", "rad", "dem", "ai", "bio4", "bio15", "gdd3","slope", "cell")], "quadratic", interaction.level= 0)
+glm1<- glm(glm.formula, data=sp_env, family= "binomial")
+
+
+# out1.glm= NULL
+# var.names<- c("bio6", "canopy", "rad", "dem", "ai", "bio4", "bio15", "gdd3","slope", "cell")
+# medians<- apply(sp_env[, var.names], 2, median)
+# medians_table<- data.frame(sapply(medians,function(x)rep(x,100)))
+# 
+# 
+# for(i in 1:10){
+#  foc.var <- sp_env[,var.names[i]]
+#  new.data <- medians_table
+#  var.new <- seq(min(foc.var), max(foc.var), length=100)
+#  new.data[,i] <- var.new
+#  pred.glm <- predict(glm1, newdata=new.data, type="response")
+#  #glm2 <- predict( glmStart, newdata=new.data, type="response")
+#  #pred.glob<- c(pred.glm,glm2)
+#  tmp1 <- cbind(Occ.prob=pred.glm,Env.val=var.new)
+#  tmp2 <- data.frame(cbind(Algorithm=rep("GLM1",each=100),
+#                           Var.name=var.names[i]))
+#  out1.glm <- rbind(out1.glm,cbind(tmp1,tmp2))
+# }
+# #plot the predictions
+# resp1.glm <- ggplot(out1.glm, aes(x=Env.val, y=Occ.prob, color=Algorithm,
+#                                   linetype=Algorithm)) +
+#  geom_line(lwd=1) +
+#  scale_color_manual(values=c("#007991", "#439A86", "#BCD8C1"))+
+#  facet_wrap(~Var.name,ncol=2,scale="free_x")+
+#  theme_bw()+
+#  labs(x="Value",y="Occurence probability")+
+#  theme_bw()
+# print(resp1.glm)
+# 
+# Pred_test <- predict(glmModAIC, calib, type="response")
+# plot(Pred_test)
+
+#create formula for virtual species manually
+inter= glm1$coefficients[1]#create the interception to y
+medians<- apply(sp_env[, var.names], 2, median)#create the medians of all variables
+medians_table<- data.frame(sapply(medians,function(x)rep(x,100)))
+median<- medians_table[1,]#have a data frame with one line
+coef<-glm1$coefficients# create a dataframe with just the coef (a in ax +b) from the glm
+#function to have h (intercept + coef*median in every other variable except the first one)
+response.variable<- function(median, coef){
+ h= as.numeric(coef[1])#h=intercept
+ median<- median[,-1]# remove bio6 from the medians
+ coef<-coef[4:length(coef)]# remove intercept and bio6 coef
+ for (i in 1:length(median)){
+  h=as.numeric(h+ coef[i*2-1]*median[i]+ coef[i*2]*median[i]^2)
+  #print(h)
+ }
+ #response_variable<- as.numeric(h + interest_coef[1]*x +interest_coef[2]*x)
+ return (h)
+}
+h= response.variable(median, coef)
+b=as.numeric(coef[2])
+c=as.numeric(coef[3])
+var1.function <- function(x, h,b,c){
+ h + b*x + c*x^2
+}
+
+response.variable2<- function(median, coef){
+ h= as.numeric(coef[1])
+ median<- median[,-2]
+ interest_coef<-coef[4:5]
+ coef<-coef[-c(1,4,5)]
+ for (i in 1:length(median)){
+  h=as.numeric(h+ coef[i*2-1]*median[i]+ coef[i*2]*median[i]^2)
+ }
+ return (h)
+}
+h2= response.variable2(median, coef)
+b2=as.numeric(coef[4])
+c2=as.numeric(coef[5])
+var2.function <- function(x, h,b,c){
+ h + b*x + c*x^2
+}
+
+
+my.responses <- formatFunctions(bio6 = c(fun = "var1.function", h=h,b=b, c=c),
+                                canopy = c(fun = "var2.function", h=h2,b=b2, c=c2))
+
+generateSpFromFun(raster.stack = my_variables[[c("bio6", "canopy")]],
+                  parameters = my.responses, plot = TRUE)
+
 
 #function to clear the data
 clear<- function(species){
