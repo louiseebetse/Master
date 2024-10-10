c2b647ebc8da575701feac84e710cadb10089437
louiseebetse <lestebe@bluewin.ch>
2024-10-10 15:05:06 +0200

    create the functions to create multiple functions

--- first species.R
+++ first species.R
@@ -23,7 +23,7 @@ my_variables <- mixedsort(list.files(path = "variables",
 my_variables<- rast(my_variables)
 cat(crs(my_variables))
 #change the names of my variables
-names(my_variables) = c("ai","bio4", "bio6", "bio15","gdd3","pop2000", "pop5000", "rad", "dem", "slope", "canopy")
+names(my_variables) = c("ai","bio4", "bio6", "bio15","gdd3","ph", "pop2000", "pop5000", "rad", "dem", "slope", "canopy")
 #compute the curvature
 my_variables$dem<-terrain(my_variables$dem, v= "TPI")
 ##plot a raster
@@ -36,16 +36,15 @@ swiss_shape <- st_transform(swiss_shape, crs = 2056)
 #export my species data from my file
 load("SPImaster/sp/1008910")
 
+## CLEAR THE DATA
 C.kitaibelii<-clear(my.sp) #clear my species data using clear()
-C.kitaibelii <- SpatialPointsDataFrame(coords=C.kitaibelii[,6:7], data=C.kitaibelii[ , -c(6, 7)]) #creating a SPDF with just the coordinates
+C.kitaibelii <- SpatialPointsDataFrame(coords=C.kitaibelii[,c("x", "y")], data=C.kitaibelii[ , -c(6, 7)]) #creating a SPDF with just the coordinates
 C.kitaibelii<- remove.duplicates(C.kitaibelii, zero = 300) #remove duplicate presence in a 300 radius?
 C.kitaibelii<- as.data.frame(C.kitaibelii) #come back to a data frame
-coord<- C.kitaibelii[,25:26]
+coord<- C.kitaibelii[,c("x", "y")]
 #plot(C.kitaibelii)
 
 
-#plot(my_variables$bio15)
-#points(C.kitaibelii)
 #extract the variables coord at the species presence
 variablesValues<-data.frame(extract(my_variables,coord))
 head(variablesValues)
@@ -53,15 +52,15 @@ head(variablesValues)
 #put the species data in the same table
 spData<- na.omit(data.frame(coord, variablesValues))
 head(spData)
-spData<-spData[,-3]#remove the id line
+spData<-spData[, !names(spData) %in% "ID"]#remove the id column
 spData$presence <- rep(1, times = length(spData$x))#put a presence line
 
 #generate background points 
 bg_rand <- terra::spatSample(my_variables$ai, 10000, "random", na.rm=T, as.points=TRUE)
-plot(swiss_shape$geom,col='lightgrey')
-points(coord,pch='+',col='red')# one way to plot presences
-points(C.kitaibelii$x, C.kitaibelii$y, col='red',  pch=19) #two ways to plot presences
-points(bg_rand,pch=13,cex=0.3)
+#plot(swiss_shape$geom,col='lightgrey')
+#points(coord,pch='+',col='red')# one way to plot presences
+#points(C.kitaibelii$x, C.kitaibelii$y, col='red',  pch=19) #two ways to plot presences
+#points(bg_rand,pch=13,cex=0.3)
 
 #create a dataframe with presences and pseudo absences
 sp_env <- data.frame(coord, occ=1)# create dataframe with presence
@@ -69,71 +68,77 @@ bg_rand_buf_df <- data.frame(terra::geom(bg_rand)[,c('x','y')])# create datafram
 bg_rand_buf_df$occ <- 0 #add the 0 to the dataframe with absences
 sp_env <- rbind(sp_env, bg_rand_buf_df) #bind the two dataframe in one big 
 sp_env <- cbind(sp_env, terra::extract(x = my_variables , y = sp_env[,c('x','y')], cells=T) )
-
-#sp_env<-sp_env[,-9]
+sp_env<-sp_env[, !names(sp_env) %in% c("ID","cell", "pop2000", "pop5000", "dem")]
 sp_env <- na.omit(sp_env)# remove the NA of the data
+
+
 #test<- sp_env[c(1:200,900:1100),]
 #args<- cov.sel(T=sp_env$occ, Y= sp_env$x , X= sp_env[,4:14], type = "np", alpha = 0.3)
 #glmModAIC<-stepAIC(glmStart, glm.formula, direction= "both", trace= FALSE, k=2)
 #summary(args)
 
 #create the glm
-glm.formula<- bm_MakeFormula("sp_env$occ",sp_env[, c("bio6", "canopy", "rad", "dem", "ai", "bio4", "bio15", "gdd3","slope", "cell")], "quadratic", interaction.level= 0)
+glm.formula<- bm_MakeFormula("sp_env$occ",sp_env[, c("bio6", "canopy", "rad", "ph", "ai", "bio4", "bio15", "gdd3","slope")], "quadratic", interaction.level= 0)
 glm1<- glm(glm.formula, data=sp_env, family= "binomial")
 
 
-# out1.glm= NULL
-# var.names<- c("bio6", "canopy", "rad", "dem", "ai", "bio4", "bio15", "gdd3","slope", "cell")
-# medians<- apply(sp_env[, var.names], 2, median)
-# medians_table<- data.frame(sapply(medians,function(x)rep(x,100)))
-# 
-# 
-# for(i in 1:10){
-#  foc.var <- sp_env[,var.names[i]]
-#  new.data <- medians_table
-#  var.new <- seq(min(foc.var), max(foc.var), length=100)
-#  new.data[,i] <- var.new
-#  pred.glm <- predict(glm1, newdata=new.data, type="response")
-#  #glm2 <- predict( glmStart, newdata=new.data, type="response")
-#  #pred.glob<- c(pred.glm,glm2)
-#  tmp1 <- cbind(Occ.prob=pred.glm,Env.val=var.new)
-#  tmp2 <- data.frame(cbind(Algorithm=rep("GLM1",each=100),
-#                           Var.name=var.names[i]))
-#  out1.glm <- rbind(out1.glm,cbind(tmp1,tmp2))
-# }
-# #plot the predictions
-# resp1.glm <- ggplot(out1.glm, aes(x=Env.val, y=Occ.prob, color=Algorithm,
-#                                   linetype=Algorithm)) +
-#  geom_line(lwd=1) +
-#  scale_color_manual(values=c("#007991", "#439A86", "#BCD8C1"))+
-#  facet_wrap(~Var.name,ncol=2,scale="free_x")+
-#  theme_bw()+
-#  labs(x="Value",y="Occurence probability")+
-#  theme_bw()
-# print(resp1.glm)
-# 
-# Pred_test <- predict(glmModAIC, calib, type="response")
-# plot(Pred_test)
-
 #create formula for virtual species manually
+var.names<-c("bio6", "canopy", "rad", "ph", "ai", "bio4", "bio15", "gdd3","slope")
 inter= glm1$coefficients[1]#create the interception to y
 medians<- apply(sp_env[, var.names], 2, median)#create the medians of all variables
 medians_table<- data.frame(sapply(medians,function(x)rep(x,100)))
 median<- medians_table[1,]#have a data frame with one line
 coef<-glm1$coefficients# create a dataframe with just the coef (a in ax +b) from the glm
 #function to have h (intercept + coef*median in every other variable except the first one)
-response.variable<- function(median, coef){
- h= as.numeric(coef[1])#h=intercept
- median<- median[,-1]# remove bio6 from the medians
- coef<-coef[4:length(coef)]# remove intercept and bio6 coef
- for (i in 1:length(median)){
-  h=as.numeric(h+ coef[i*2-1]*median[i]+ coef[i*2]*median[i]^2)
-  #print(h)
+
+#function to create the different 
+function_creator <- function(median, coef, var_names){
+ funcs <- vector("list", length(var_names))
+ H<-c()
+ B<-c()
+ C<-c()
+ for (i in 1:length(var_names)) {
+  interest_variable = var_names[i]
+  h= as.numeric(coef[1])
+  pro_median<- median[, !names(median) %in% interest_variable]# remove bio6 from the medians
+  pro_coef<-coef[!names(coef) %in% c("(Intercept)", interest_variable, paste0("I(", interest_variable, "^2)"))]# remove intercept and bio6 coef
+  for (j in 1:length(pro_median)){
+   h=as.numeric(h+ pro_coef[j*2-1]*median[j]+ pro_coef[j*2]*median[j]^2)
+  }
+  H<- c(H, h)
+  B=c(B, as.numeric(coef[2*i]))
+  C=c(C, as.numeric(coef[2*i+1]))
+  funcs[[i]] <- function(x,h,b,c) {
+   h + b*x + c*x^2
+  }
  }
- #response_variable<- as.numeric(h + interest_coef[1]*x +interest_coef[2]*x)
+ df<- data.frame(variable= var_names, h= H, b= B, c=C, stringsAsFactors = FALSE)
+ df$functions <- funcs
+ row.names(df)<-var_names
+ return(df)
+}
+
+fun1<- df$functions[[1]]
+fun2<- df$functions[[2]]
+fun3<- df$functions[[3]]
+fun4<- df$functions[[4]]
+fun5<- df$functions[[5]]
+fun6<- df$functions[[6]]
+fun7<- df$functions[[7]]
+fun8<- df$functions[[8]]
+fun9<- df$functions[[9]]
+
+response.variable<- function(median, coef, interest_variable){
+ h= as.numeric(coef[1])#h=intercept
+ median<- median[, !names(median) %in% interest_variable]# remove bio6 from the medians
+ coef<-coef[!names(coef) %in% c("(Intercept)", interest_variable, paste0("I(", interest_variable, "^2)"))]# remove intercept and bio6 coef
+ for (i in 1:length(pro_median)){
+  h=as.numeric(h+ coef[i*2-1]*median[i]+ coef[i*2]*median[i]^2)
+ }
  return (h)
 }
-h= response.variable(median, coef)
+
+h= response.variable(median, coef, "bio6")
 b=as.numeric(coef[2])
 c=as.numeric(coef[3])
 var1.function <- function(x, h,b,c){
@@ -158,8 +163,15 @@ var2.function <- function(x, h,b,c){
 }
 
 
-my.responses <- formatFunctions(bio6 = c(fun = "var1.function", h=h,b=b, c=c),
-                                canopy = c(fun = "var2.function", h=h2,b=b2, c=c2))
+my.responses <- formatFunctions(bio6 = c(fun = 'test1', h=df$h[1],b=df$b[1], c=df$c[1]),
+                                canopy = c(fun = 'test2', h=df$h[2],b=df$b[2], c=df$c[2]),
+                                rad = c(fun = 'test2', h=df$h[3],b=df$b[3], c=df$c[3]),
+                                ph = c(fun = 'test2', h=df$h[4],b=df$b[4], c=df$c[4]),
+                                ai = c(fun = 'test2', h=df$h[5],b=df$b[5], c=df$c[5]),
+                                bio4 = c(fun = 'test2', h=df$h[6],b=df$b[6], c=df$c[6]),
+                                bio15 = c(fun = 'test2', h=df$h[7],b=df$b[7], c=df$c[]),
+                                gdd3 = c(fun = 'test2', h=df$h[8],b=df$b[8], c=df$c[1]),
+                                slope = c(fun = 'test2', h=df$h[9],b=df$b[9], c=df$c[1]),)
 
 generateSpFromFun(raster.stack = my_variables[[c("bio6", "canopy")]],
                   parameters = my.responses, plot = TRUE)
