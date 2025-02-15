c20e5897dfa57deee087f14c45f702d01a2ac7ca
louiseebetse <162605099+louiseebetse@users.noreply.github.com>
2025-02-15 11:18:09 +0100

    Update random_species.R

--- random_species.R
+++ random_species.R
@@ -29,27 +29,35 @@ random.sp <- generateRandomSp(my_variables, approach = "response",
 saveRDS(random.sp, "saved_data/random_species/response_realistic.RDS")                              
 
 
-random.sp3<- generateRandomSp(my_variables, approach = "response", 
-                              realistic.sp = TRUE, 
-                              niche.breadth = 'narrow',
-                              convert.to.PA = TRUE)
-saveRDS(random.sp3, "saved_data/random_species/response_realistic_narrow.RDS")                              
+# random.sp3<- generateRandomSp(my_variables, approach = "response", 
+#                               realistic.sp = TRUE, 
+#                               niche.breadth = 'narrow',
+#                               convert.to.PA = TRUE)
+# saveRDS(random.sp3, "saved_data/random_species/response_realistic_narrow.RDS")                              
+#narrow makes a suitability map too small so it does not work bc it has then too many points
 
 random.sp4<- generateRandomSp(my_variables, approach = "response", 
                               realistic.sp = TRUE, 
                               niche.breadth = 'wide',
                               convert.to.PA = TRUE)
-saveRDS(random.sp4, "saved_data/random_species/response_realistic_narrow.RDS")                              
+saveRDS(random.sp4, "saved_data/random_species/response_realistic_wide.RDS")                              
  
 #######################################################################
-#Population and Observations
+#Populations
 #######################################################################
                              
 #randomly select half of the popu from my random distributions
 #new_random_popu<-spatSample(response_realistic_narrow$pa.raster, size = global(response_realistic_narrow$pa.raster == 1, fun = "sum", na.rm = TRUE)$sum/2 ,
                         #method= "weights", na.rm= TRUE, as.points = TRUE)
-random_total_popu<- as.points(response_realistic_narrow$pa.raster == 1, values=FALSE)
-sample_size<-global(response_realistic_narrow$pa.raster == 1, fun = "sum", na.rm = TRUE)$sum/2 
+response_realistic <- readRDS("~/Desktop/UNIL/Master/Rstudio/Virtual species/saved_data/random_species/response_realistic.RDS")
+response_realistic$pa.raster<-terra::unwrap(response_realistic$pa.raster)
+response_realistic$suitab.raster<-terra::unwrap(response_realistic$suitab.raster)
+
+random_total_popu<- as.points(response_realistic$pa.raster == 1, values=FALSE)
+saveRDS(random_total_popu, "saved_data/random_species/random_total_popu.RDS")
+sample_size<-global(response_realistic$pa.raster == 1, fun = "sum", na.rm = TRUE)$sum%/%2 
+saveRDS(sample_size, "saved_data/random_species/sample_size.RDS" )
+suitab_random<-response_realistic$suitab.raster
 
 #STABLE POPU
 stady_stady_sample<- function(repetition, sample_size,sample_vector,proba_raster){
@@ -62,15 +70,18 @@ stady_stady_sample<- function(repetition, sample_size,sample_vector,proba_raster
  col <- colorRampPalette(c(start_color, end_color))(repetition)
  indices_to_keep<-sample(nrow(sample_vector), size = sample_size, 
                          prob = weights, replace = FALSE)# sample the individuals with weights
- year<-sample2[indices_to_keep,]#recreate a spatvector from the desired individuals
+ year<-sample_vector[indices_to_keep,]#recreate a spatvector from the desired individuals
  for(i in 1:repetition){
   year=year
   points(year, col= col[i], pch= 20, cex =0.5)#make points on the map
-  saveRDS(year, paste("saved_data/stady_stady_sample/year",i,".RDS", sep=""))#save each year in a file
+  saveRDS(year, paste("saved_data/random_species/stady_stady_sample/year",i,".RDS", sep=""))#save each year in a file
  }
  return(invisible(year))
 }
-stady_stady_sample(6,sample_size,new_random_popu,random.sp$pa.raster)
+stady_stady_sample(6,200,random_total_popu,suitab_random )
+#i put 200 for the sample size but it should be half of the number of point
+#instead of 200 put sample_size
+# should we do less than half should i select points from the random suitab map
 
 #INCREASE POPU
 increase_sample<- function(repetition, sample_size,sample_vector,proba_raster, max_increase){
@@ -82,7 +93,7 @@ increase_sample<- function(repetition, sample_size,sample_vector,proba_raster, m
  year<-sample_vector[indices_to_keep,]#recreate a spatvector from the desired individuals
  new_weights<-weights
  new_weights[indices_to_keep]<-0
- saveRDS(year, "saved_data/increase_sample/year1.RDS")
+ saveRDS(year, "saved_data/random_species/increase_sample/year1.RDS")
  
  num_to_add <- (max_increase/(repetition-1))/100*sample_size
  
@@ -104,7 +115,7 @@ increase_sample<- function(repetition, sample_size,sample_vector,proba_raster, m
  }
  return(invisible(year))
 }
-increase_sample(6,1000,new_random_popu,random.sp$pa.raster,50)
+increase_sample(6,200,random_total_popu,suitab_random,45)
 #DECREASE POPU
 decrease_sample<- function(repetition, sample_size,sample_vector,proba_raster, max_reduce){
  #set.seed(42)
@@ -115,7 +126,7 @@ decrease_sample<- function(repetition, sample_size,sample_vector,proba_raster, m
                          prob = weights, replace = FALSE)
  year<-sample_vector[indices_to_keep,]#recreate a spatvector from the desired individuals
  new_inverse_weights<-inverse_weights[indices_to_keep]
- saveRDS(year, "saved_data/decrease_sample/year1.RDS")
+ saveRDS(year, "saved_data/random_species/decrease_sample/year1.RDS")
  
  num_to_delete <- (max_reduce/(repetition-1))/100*sample_size
  
@@ -136,28 +147,39 @@ decrease_sample<- function(repetition, sample_size,sample_vector,proba_raster, m
  }
  return(invisible(year))
 }
-decrease_sample(6,1000,new_random_popu,random.sp$pa.raster,50)
+decrease_sample(6,200,random_total_popu,suitab_random,50)
 
 
-#load the species 
-load("SPImaster/sp/1008910")
-# Convert the "date" column to Date format if it's not already
-my.sp$date <- as.Date(my.sp$date)
+#################################################################################
+#Observations
+#################################################################################
 
-# Define the date breaks
-breaks <- as.Date(c("1994-12-31", "2000-12-31", "2006-12-31", 
-                    "2012-12-31", "2018-12-31", "2024-12-31"))
+my_biais <- mixedsort(list.files(path = "biais",
+                                 pattern = NULL,
+                                 full.names = TRUE))
+my_biais<- rast(my_biais)
+head(my_biais)
+names(my_biais)<-c("obs_Origin_1994", "obs_1995_2000", "obs_2001_2006", "obs_2007_2012",
+                   "obs_2013_2018", "obs_2019_2024",
+                   "Nobsvs_Origin_1994", "Nobsvs_1995_2000", "Nobsvs_2001_2006", "Nobsvs_2007_2012",
+                   "Nobsvs_2013_2018", "Nobsvs_2019_2024",
+                   "prop_pot_Origin-1994", "prop_pot_1995-2000", "prop_pot_2001-2006",  
+                   "prop_pot_2007-2012", "prop_pot_2013-2018", "prop_pot_2019-2024")
 
-# Use cut() to classify the dates into groups
-categories <- cut(my.sp$date, breaks = c(as.Date("1900-01-01"), breaks), 
-                  labels = c("Before 1994", "1995-2000", "2001-2006", 
-                             "2007-2012", "2013-2018", "2019-2024"), 
-                  right = TRUE)
+#normalised the biais
+biais_transformed<- my_biais
+for (i in 1:length(names(my_biais))){
+ biais_transformed[[i]]<-log(my_biais[[i]] + 1)
+}
 
-#Count the number of observations in each category &
-#Convert to a numeric vector
-date_vector <- as.numeric(table(categories))
-
+#count the total number of observations per raster per year
+nb_obs<-data.frame(obs_Origin_1994=0, obs_1995_2000= 0, obs_2001_2006=0, obs_2007_2012=0, obs_2013_2018=0, obs_2019_2024=0)
+for (i in c(1:6)){
+ total_sum <- global(my_biais[[i]], fun = "sum", na.rm = TRUE)$sum
+ nb_obs[,i]<-total_sum
+}
+#I decrease the number of sample to see if it's work otherwise it will take too long
+nb_obs<-nb_obs%/%100
 
 #STABLE OBSERVATIONS 90%
 biais_stady_sample<- function(repetition, sample_size, sample_vector,proba_raster, biais_raster){
@@ -171,7 +193,7 @@ biais_stady_sample<- function(repetition, sample_size, sample_vector,proba_raste
  col <- colorRampPalette(c(start_color, end_color))(repetition)
  indices_to_keep<-sample(nrow(sample_vector), size = sample_size, 
                          prob = weights, replace = FALSE)# sample the individuals with weights
- year<-sample2[indices_to_keep,]#recreate a spatvector from the desired individuals
+ year<-sample_vector[indices_to_keep,]#recreate a spatvector from the desired individuals
  for(i in 1:repetition){
   year=year
   par(mfg = c(1, 1))
@@ -179,7 +201,7 @@ biais_stady_sample<- function(repetition, sample_size, sample_vector,proba_raste
   saveRDS(year, paste("saved_data/random_species/biais_stable/09_year",i,".RDS", sep=""))#save each year in a file
   
   biais_weights<- terra::extract(biais_raster[[i]], year, xy= TRUE)[, 2]
-  biais_year<-sample(year, size = 0.9*date_vector[i], prob = biais_weights, replace = TRUE)
+  biais_year<-sample(year, size = 0.9*nb_obs[,i], prob = biais_weights, replace = TRUE)
   unique_biais_year <- terra::unique(biais_year)
   par(mfg = c(1, 2))
   points(unique_biais_year, col= col[i], pch= 20, cex =0.5)#make points on the map
@@ -188,7 +210,7 @@ biais_stady_sample<- function(repetition, sample_size, sample_vector,proba_raste
  }
  return(invisible(year))
 }
-#biais_stady_sample(6,popu300,sample2,prob_raster_quantile,biais_transformed)
+biais_stady_sample(6,1000,random_total_popu,suitab_random,biais_transformed)
 
 #INCREASE POPULATION WITH OBSERVATIONS 90%
 biais_increase_sample<- function(repetition, sample_size,sample_vector,proba_raster, max_increase,biais_raster){
@@ -204,7 +226,7 @@ biais_increase_sample<- function(repetition, sample_size,sample_vector,proba_ras
  saveRDS(year, "saved_data/random_species/biais_increase/09_year1.RDS")
  
  biais_weights<- terra::extract(biais_raster[[1]], year, xy= TRUE)[, 2]
- biais_year<-sample(year, size = 0.9*date_vector[1], prob = biais_weights, replace = TRUE)
+ biais_year<-sample(year, size = 0.9*nb_obs[,1], prob = biais_weights, replace = TRUE)
  unique_biais_year <- terra::unique(biais_year)
  saveRDS(unique_biais_year, paste("saved_data/random_species/biais_increase/09_biais_year1.RDS", sep=""))#save each year in a file
  
@@ -237,7 +259,7 @@ biais_increase_sample<- function(repetition, sample_size,sample_vector,proba_ras
   
   biais_weights<- terra::extract(biais_raster[[i+1]], year, xy= TRUE)[, 2]
   #biais_year<-sample(year, size = sample_size+i*num_to_add, prob = biais_weights, replace = TRUE)
-  biais_year<-sample(year, size = 0.9*date_vector[i+1], prob = biais_weights, replace = TRUE)
+  biais_year<-sample(year, size = 0.9*nb_obs[,i+1], prob = biais_weights, replace = TRUE)
   unique_biais_year <- terra::unique(biais_year)
   par(mfg = c(1, 2))
   points(unique_biais_year, col= col[i+1], pch= 20, cex =0.5)#make points on the map
@@ -260,7 +282,7 @@ biais_decrease_sample<- function(repetition, sample_size,sample_vector,proba_ras
  saveRDS(year, "saved_data/random_species/biais_decrease/09_year1.RDS")
  
  biais_weights<- terra::extract(biais_raster[[1]], year, xy= TRUE)[, 2]
- biais_year<-sample(year, size = 0.9*date_vector[1], prob = biais_weights, replace = TRUE)
+ biais_year<-sample(year, size = 0.9*nb_obs[,1], prob = biais_weights, replace = TRUE)
  unique_biais_year <- terra::unique(biais_year)
  saveRDS(unique_biais_year, paste("saved_data/random_species/biais_decrease/09_biais_year1.RDS", sep=""))#save each year in a file
  
@@ -292,7 +314,7 @@ biais_decrease_sample<- function(repetition, sample_size,sample_vector,proba_ras
   
   biais_weights<- terra::extract(biais_raster[[i+1]], year, xy= TRUE)[, 2]
   #biais_year<-sample(year, size = sample_size-i*num_to_delete, prob = biais_weights, replace = TRUE)
-  biais_year<-sample(year, size = 0.9*date_vector[i+1], prob = biais_weights, replace = TRUE)
+  biais_year<-sample(year, size = 0.9*nb_obs[,i+1], prob = biais_weights, replace = TRUE)
   unique_biais_year <- terra::unique(biais_year)
   par(mfg = c(1, 2))
   points(unique_biais_year, col= col[i+1], pch= 20, cex =0.5)#make points on the map
@@ -315,7 +337,7 @@ biais_stady_sample<- function(repetition, sample_size, sample_vector,proba_raste
  col <- colorRampPalette(c(start_color, end_color))(repetition)
  indices_to_keep<-sample(nrow(sample_vector), size = sample_size, 
                          prob = weights, replace = FALSE)# sample the individuals with weights
- year<-sample2[indices_to_keep,]#recreate a spatvector from the desired individuals
+ year<-sample_size[indices_to_keep,]#recreate a spatvector from the desired individuals
  for(i in 1:repetition){
   year=year
   par(mfg = c(1, 1))
@@ -323,7 +345,7 @@ biais_stady_sample<- function(repetition, sample_size, sample_vector,proba_raste
   saveRDS(year, paste("saved_data/random_species/biais_stable/01_year",i,".RDS", sep=""))#save each year in a file
   
   biais_weights<- terra::extract(biais_raster[[i]], year, xy= TRUE)[, 2]
-  biais_year<-sample(year, size = 0.1*date_vector[i], prob = biais_weights, replace = TRUE)
+  biais_year<-sample(year, size = 0.1*nb_obs[,i], prob = biais_weights, replace = TRUE)
   unique_biais_year <- terra::unique(biais_year)
   par(mfg = c(1, 2))
   points(unique_biais_year, col= col[i], pch= 20, cex =0.5)#make points on the map
@@ -348,7 +370,7 @@ biais_increase_sample<- function(repetition, sample_size,sample_vector,proba_ras
  saveRDS(year, "saved_data/random_species/biais_increase/01_year1.RDS")
  
  biais_weights<- terra::extract(biais_raster[[1]], year, xy= TRUE)[, 2]
- biais_year<-sample(year, size = 0.1*date_vector[1], prob = biais_weights, replace = TRUE)
+ biais_year<-sample(year, size = 0.1*nb_obs[,1], prob = biais_weights, replace = TRUE)
  unique_biais_year <- terra::unique(biais_year)
  saveRDS(unique_biais_year, paste("saved_data/random_species/biais_increase/01_biais_year1.RDS", sep=""))#save each year in a file
  
@@ -381,7 +403,7 @@ biais_increase_sample<- function(repetition, sample_size,sample_vector,proba_ras
   
   biais_weights<- terra::extract(biais_raster[[i+1]], year, xy= TRUE)[, 2]
   #biais_year<-sample(year, size = sample_size+i*num_to_add, prob = biais_weights, replace = TRUE)
-  biais_year<-sample(year, size = 0.1*date_vector[i+1], prob = biais_weights, replace = TRUE)
+  biais_year<-sample(year, size = 0.1*nb_obs[,i+1], prob = biais_weights, replace = TRUE)
   unique_biais_year <- terra::unique(biais_year)
   par(mfg = c(1, 2))
   points(unique_biais_year, col= col[i+1], pch= 20, cex =0.5)#make points on the map
@@ -404,7 +426,7 @@ biais_decrease_sample<- function(repetition, sample_size,sample_vector,proba_ras
  saveRDS(year, "saved_data/random_species/biais_decrease/01_year1.RDS")
  
  biais_weights<- terra::extract(biais_raster[[1]], year, xy= TRUE)[, 2]
- biais_year<-sample(year, size = 0.1*date_vector[1], prob = biais_weights, replace = TRUE)
+ biais_year<-sample(year, size = 0.1*nb_obs[,1], prob = biais_weights, replace = TRUE)
  unique_biais_year <- terra::unique(biais_year)
  saveRDS(unique_biais_year, paste("saved_data/random_species/biais_decrease/01_biais_year1.RDS", sep=""))#save each year in a file
  
@@ -436,7 +458,7 @@ biais_decrease_sample<- function(repetition, sample_size,sample_vector,proba_ras
   
   biais_weights<- terra::extract(biais_raster[[i+1]], year, xy= TRUE)[, 2]
   #biais_year<-sample(year, size = sample_size-i*num_to_delete, prob = biais_weights, replace = TRUE)
-  biais_year<-sample(year, size = 0.1*date_vector[i+1], prob = biais_weights, replace = TRUE)
+  biais_year<-sample(year, size = 0.1*nb_obs[,i+1], prob = biais_weights, replace = TRUE)
   unique_biais_year <- terra::unique(biais_year)
   par(mfg = c(1, 2))
   points(unique_biais_year, col= col[i+1], pch= 20, cex =0.5)#make points on the map
